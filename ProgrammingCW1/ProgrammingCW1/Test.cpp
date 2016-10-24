b3931496a514fce72db678e29fa1340b6fcda26f
Dropzee <jackcox@ntlworld.com>
2016-10-24 14:35:17 +0100

    [_/] Compare finished, 49 unique files

--- ProgrammingCW1/ProgrammingCW1/Test.cpp
+++ ProgrammingCW1/ProgrammingCW1/Test.cpp
@@ -2,31 +2,28 @@
 #include "Encoder.h"
 #include "FileIO.h"
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void generator(string* outputs, string input, Encoder* e, FileIO* IO) {
-	int count = 0;
+void generator(vector<string> outputs, string input, Encoder* e, FileIO* IO) {
+	string temp;
 	for (int xor1a = 0; xor1a < 4; xor1a++) {
 		for (int xor1b = 0; xor1b < 4; xor1b++) {
 			for (int xor2a = 0; xor2a < 4; xor2a++) {
 				for (int xor2b = 0; xor2b < 4; xor2b++) {
-					outputs[count] = e->encode(input, xor1a, xor1b, xor2a, xor2b);
-					IO->write(outputs[count], "XOR1(" + to_string(xor1a) + "," + to_string(xor1b) + ") XOR2(" + to_string(xor2a) + "," + to_string(xor2b) + ")");
-					count++;
-				}
-			}
-		}
-	}
-}
-
-void compare(string* outputs) {
-	for (int i = 0; i < 256; i++) {
-		vector<int> matches;
-		for (int j = 0; j < 256; j++) {
-			if (i != j) {
-				if (outputs[i].compare(outputs[j]) == 0) {
-					matches.push_back(j);
+					temp = e->encode(input, xor1a, xor1b, xor2a, xor2b);
+					bool unique = true;
+					for each(string s in outputs) {
+						if (s.compare(temp) == 0) {
+							unique = false;
+							break;
+						}
+					}
+					if (unique) {
+						outputs.push_back(temp);
+						IO->write(temp, "XOR1(" + to_string(xor1a) + "," + to_string(xor1b) + ") XOR2(" + to_string(xor2a) + "," + to_string(xor2b) + ")");
+					}
 				}
 			}
 		}
@@ -36,7 +33,7 @@ void compare(string* outputs) {
 int main() {
 
 	string input;
-	string outputs[256];
+	vector<string> outputs;
 
 	FileIO* IO = new FileIO();
 	Encoder* e = new Encoder();
@@ -49,7 +46,7 @@ int main() {
 		exit(1);
 	}
 
-	generator(&outputs[0], input, e, IO);
+	generator(outputs, input, e, IO);
 
 	system("pause");
 
