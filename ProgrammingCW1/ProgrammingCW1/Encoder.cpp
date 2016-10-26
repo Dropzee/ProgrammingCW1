2fa204f874aca35de836ae8c7fe313ee6a19094a
Dropzee <jackcox@ntlworld.com>
2016-10-26 17:33:56 +0100

    [] Fixed outputs

--- ProgrammingCW1/ProgrammingCW1/Encoder.cpp
+++ ProgrammingCW1/ProgrammingCW1/Encoder.cpp
@@ -13,26 +13,27 @@ Encoder::~Encoder()
 string Encoder::encode(string input, int xor1a, int xor1b, int xor2a, int xor2b)
 {
 	//Append 000 as initial register state
-	input += "000";
+	input = string(input.rbegin(), input.rend()) + "000";
 
 	//Set up pointer to registers
 	char* registers = &input[input.length() - 4];
 
 	//Initalise output string to allow appending
-	string output = "";
+	string output;
 
 	for (int i = 0; i <= (input.length() - 4); i++) {
 
 		//XOR & add to output
-		output += XOR(*(registers + xor1a), *(registers + xor1b));
+
 		output += XOR(*(registers + xor2a), *(registers + xor2b));
+		output += XOR(*(registers + xor1a), *(registers + xor1b));
 
 		//Shift registers by moving pointer
 		registers--;
 	}
 
 	//Reverse bit order
-	return string(output.rbegin(), output.rend());
+	return output;
 }
 
 //XOR Method
